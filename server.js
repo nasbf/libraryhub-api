08c61bb1136c136754627bf170e3b8ae110974b0
nasbf <mo0ma@hotmail.com>
2026-03-31 17:31:18 -0500

    add database

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
+require('dotenv').config();
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.json());
+
+const port = process.env.PORT || 8080;
+
+mongoose.connect(process.env.MONGODB_URL)
+  .then(() => console.log("MongoDB conected"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("LibraryHub API running");
