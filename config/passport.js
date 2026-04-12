50ec25930cbea03fccfb7823080a88f450ccde00
nasbf <145506005+nasbf@users.noreply.github.com>
2026-04-12 01:05:45 -0500

    add consolelog.
    Added a variable for the Google OAuth callback URL and logged it.

--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,15 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/users');
 
+const callback = `${process.env.BASE_URL}/auth/google/callback`;
+console.log(" CALLBACK REAL:", callback);
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: callback
+},
+                                
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
